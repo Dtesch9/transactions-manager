6e4740891ee788378011e573b081f437a66fcbeb
Dtesch9 <alucard.s8123@gmail.com>
2020-04-16 16:56:31 -0300

    database connection

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,13 @@
+import 'reflect-metadata';
+import 'dotenv/config';
+
 import express from 'express';
+
 import routes from './routes';
 
+import createConnection from './database';
+
+createConnection();
 const app = express();
 
 app.use(express.json());
